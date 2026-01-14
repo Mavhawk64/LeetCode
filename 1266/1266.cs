81d2bdb0591e26edba1e5c6f236ce47e7db5d19a
Maverick Berkland <mavberk@gmail.com>
2026-01-13 21:04:44 -0500

    Added names/availables_languages for random language pulling

---
+++ 1266/1266.cs
@@ -0,0 +1,38 @@
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("LeetCode 1266");
+// [[1,1],[3,4],[-1,0]]
+// [[3,2],[-2,2]]
+var solution = new Solution();
+var test1 = new int[][]
+{
+    [1,1],
+    [3,4],
+    [-1,0]
+};
+
+var test2 = new int[][]
+{
+    [3,2],
+    [-2,2]
+};
+var result1 = solution.MinTimeToVisitAllPoints(test1);
+Console.WriteLine(result1);
+var result2 = solution.MinTimeToVisitAllPoints(test2);
+Console.WriteLine(result2);
+public class Solution
+{
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        int time = 0;
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            int diag = Math.Min(Math.Abs(points[i][0] - points[i + 1][0]), Math.Abs(points[i][1] - points[i + 1][1]));
+            int straight = Math.Abs(points[i][0] - points[i + 1][0]) + Math.Abs(points[i][1] - points[i + 1][1]) - diag * 2;
+            time += diag + straight;
+        }
+        return time;
+    }
+}
